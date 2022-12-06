7336beff03ae6b32e8cf3959096d08a776565a57
juleni <69912179+juleni@users.noreply.github.com>
2022-12-06 11:02:15 +0100

    Added Dictaphone component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Dictaphone from "./Dictaphone.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">DICTAPHONE</header>
+      <header className="App-header">
+        DICTAPHONE
+        <Dictaphone />
+      </header>
     </div>
   );
 }
