98161f43bb53126e117ff23c0cfa8fbfb0f9b314
juleni <69912179+juleni@users.noreply.github.com>
2022-12-06 19:19:35 +0100

    Added header/ microphone/transcript

--- src/App.js
+++ src/App.js
@@ -1,15 +1,103 @@
+import { useRef, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import "./App.css";
-import Dictaphone from "./Dictaphone.js";
+import Header from "./components/Header";
+import microPhoneIcon from "./microphone.svg";
 
 function App() {
+  const commands = [
+    {
+      command: "open *",
+      callback: (website) => {
+        window.open("http://" + website.split(" ").join(""));
+      },
+    },
+    {
+      command: "change background color to *",
+      callback: (color) => {
+        document.body.style.background = color;
+      },
+    },
+    {
+      command: "reset",
+      callback: () => {
+        handleReset();
+      },
+    },
+    ,
+    {
+      command: "reset background color",
+      callback: () => {
+        document.body.style.background = `rgba(0, 0, 0, 0.8)`;
+      },
+    },
+  ];
+
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+  const [isListening, setIsListening] = useState(false);
+  const microphoneRef = useRef(null);
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+  const handleListing = () => {
+    setIsListening(true);
+    console.log("CL");
+    console.log(microphoneRef.current.classList);
+    microphoneRef.current.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: true,
+    });
+  };
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+  const handleReset = () => {
+    stopHandle();
+    resetTranscript();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        DICTAPHONE
-        <Dictaphone />
-      </header>
+    <div className="main-container">
+      {/** Header */}
+      <Header />
+      {/** Microphone */}
+      <div className="microphone-wrapper">
+        <div className="mircophone-container">
+          <div
+            className="microphone-icon-container"
+            ref={microphoneRef}
+            onClick={handleListing}
+          >
+            <img src={microPhoneIcon} className="microphone-icon" />
+          </div>
+          <div className="microphone-status">
+            {isListening
+              ? "Click to STOP Listening"
+              : "Click to START Listening"}
+          </div>
+          {isListening && (
+            <button className="microphone-stop btn" onClick={stopHandle}>
+              Stop
+            </button>
+          )}
+        </div>
+        {transcript && (
+          <div className="microphone-result-container">
+            <div className="microphone-result-text">{transcript}</div>
+            <button className="microphone-reset btn" onClick={handleReset}>
+              Reset
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h1>D I C T A P H O N E</h1>
+    </div>
+  );
+}
